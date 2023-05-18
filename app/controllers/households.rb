75638fc9d17ccfa87e2741a6e2706ffa7b22dfc3
Seamonster541 <se_eeds@yahoo.com>
2023-05-19 05:55:38 +0800

    added household to APP - controllers and services

---
+++ app/controllers/households.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'roda'
+
+module OnlineCheckIn
+  # Web controller for OnlineCheckin API
+  class App < Roda
+    route('households') do |routing|
+      routing.on do
+        # GET /households/
+        routing.get do
+          if @current_account.logged_in?
+            household_list = GetAllProjects.new(App.config).call(@current_account)
+
+            households = Projects.new(household_list)
+
+            view :households_all,
+                 locals: { current_user: @current_account, households: }
+          else
+            routing.redirect '/auth/login'
+          end
+        end
+      end
+    end
+  end
+end
