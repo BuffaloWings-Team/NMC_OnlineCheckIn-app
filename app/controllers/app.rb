9b63f449ebb9930f4454ab235a8dbe9c2e1fbc14
Borhan Lee <BorhanLee10@gmail.com>
2023-05-21 19:20:51 +0800

    bug fixed

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -15,7 +15,7 @@ module OnlineCheckIn
 
     route do |routing|
       response['Content-Type'] = 'text/html; charset=utf-8'
-      @current_account = CurrentSession.new(session).current_account
+      @current_account = SecureSession.new(session).get(:current_account)
 
       routing.public
       routing.assets
