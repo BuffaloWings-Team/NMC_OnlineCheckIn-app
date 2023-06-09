41f833fb06211aa21c13d38706a86780b9802c68
Borhan Lee <BorhanLee10@gmail.com>
2023-06-09 17:27:23 +0800

    add some features

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -15,7 +15,6 @@ module OnlineCheckIn
     end
 
     route('auth') do |routing|
-      @oauth_callback = '/auth/sso_callback'
       @login_route = '/auth/login'
       routing.is 'login' do
         # GET /auth/login
@@ -48,7 +47,7 @@ module OnlineCheckIn
         rescue AuthenticateAccount::NotAuthenticatedError
           flash[:error] = 'Username and password did not match our records'
           response.status = 401
-          routing.redirect @login_route
+          view :login
         rescue AuthenticateAccount::ApiServerError => e
           App.logger.warn "API server error: #{e.inspect}\n#{e.backtrace}"
           flash[:error] = 'Our servers are not responding -- please try later'
--- spec/itegration/service_authenticate_spec.rb
+++ spec/itegration/service_authenticate_spec.rb
@@ -27,11 +27,11 @@ describe 'Test Service Objects' do
       auth_return_json = File.read(auth_account_file)
 
       WebMock.stub_request(:post, "#{API_URL}/auth/authenticate")
-             .with(body: @credentials.to_json)
+             .with(body: SignedMessage.sign(@credentials).to_json)
              .to_return(body: auth_return_json,
                         headers: { 'content-type' => 'application/json' })
 
-      auth = OnlineCheckIn::AuthenticateAccount.new(app.config).call(**@credentials)
+      auth = OnlineCheckIn::AuthenticateAccount.new.call(**@credentials)
 
       account = auth[:account]['attributes']
       _(account).wont_be_nil
@@ -41,7 +41,7 @@ describe 'Test Service Objects' do
 
     it 'BAD: should not find a false authenticated account' do
       WebMock.stub_request(:post, "#{API_URL}/auth/authenticate")
-             .with(body: @mal_credentials.to_json)
+             .with(body: SignedMessage.sign(@mal_credentials).to_json)
              .to_return(status: 401)
       _(proc {
         OnlineCheckIn::AuthenticateAccount.new.call(**@mal_credentials)
