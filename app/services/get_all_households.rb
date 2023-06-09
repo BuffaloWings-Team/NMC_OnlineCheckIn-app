23c536361b5426c82b6aa4c1fb394de10a82cf23
Borhan Lee <BorhanLee10@gmail.com>
2023-06-09 16:12:46 +0800

    bug fix

--- app/services/get_all_households.rb
+++ app/services/get_all_households.rb
@@ -5,14 +5,15 @@ require 'http'
 module OnlineCheckIn
   # Returns all projects belonging to an account
   class GetAllProjects
-  def initialize(config)
-    @config = config
-  end
+    def initialize(config)
+      @config = config
+    end
 
-  def call(current_account)
-    response = HTTP.auth("Bearer #{current_account.auth_token}")
-                   .get("#{@config.API_URL}/households")
+    def call(current_account)
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                    .get("#{@config.API_URL}/households")
 
-    response.code == 200 ? JSON.parse(response.to_s)['data'] : nil
+      response.code == 200 ? JSON.parse(response.to_s)['data'] : nil
+    end
   end
 end
